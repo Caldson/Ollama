538caa0e42d937280a63e1ab4da6113c8260e3cf
Caldson <minecraftcaldson@outlook.com>
2026-07-22 20:24:04 +0800

    Update easyollama.py

--- easyollama.py
+++ easyollama.py
@@ -29,10 +29,12 @@ class EasyOllama:
 
 # Pre-release
 class EasyOllamaBeta:
-    def __init__(self, api_key, model='qwen3.5:0.8b', base_url='http://localhost:11434/', chinese_mode=True): # Use qwen3.5:0.8b for best experience.
+    def __init__(self, api_key, model='qwen3.5:0.8b', base_url='http://localhost:11434/', chinese_mode=True, usingterminal=True): # Use qwen3.5:0.8b for best experience.
         self.model = model
         self.chinese_mode = chinese_mode
+        self.usingterminal = usingterminal
         self.base_url = base_url
+        self.api_key = api_key
         print(f"[EasyOllamaBeta] 初始化模型中：{model}" if self.chinese_mode else f"[EasyOllamaBeta] Init model {model}...")
         self.client = anthropic.Anthropic(
             base_url=base_url,
@@ -41,7 +43,7 @@ class EasyOllamaBeta:
         self.messages = [
             {'role': 'system', 'content': '你是一个有帮助的AI智能助手，你回复的内容必须是纯文本格式，因为使用端是Python，不能包含HTML、Markdown、原生Ollama的标签（包括left、right、'+r'\['+'、frac等等）等文本，必须是纯文本格式！。如果用户请求你回答Python等等代码，你必须编辑文件。编辑文件前必须先阅读文件内容（使用read_file这个tools然后用edit_file这个tools进行编辑），不能直接编辑（防止覆盖了重要的信息）。如果用户让你用tools什么的（编辑文件、阅读文件等等等等）你必须要用tools！'},
         ]
-    def chat(self, message, role='user'):
+    def chat(self, message, role='user', thinking=True):
         self.messages.append({'role': role, 'content': message})
         chat_completion = self.client.messages.create(
             messages=self.messages,
@@ -100,33 +102,39 @@ class EasyOllamaBeta:
         for block in chat_completion.content:
             if block.type == "tool_use":
                 if block.name == "get_time":
-                    self.get_time(block.input)
+                    self.get_time(block.input, thinking)
                 elif block.name == "edit_file":
-                    self.edit_file(block.input)
+                    self.edit_file(block.input, thinking)
                 elif block.name == "read_file":
-                    self.read_file(block.input)
+                    self.read_file(block.input, thinking)
             elif block.type == "text":
                 if block.text:
-                    self.output_text += Fore.RESET + block.text + f"\n{Fore.RESET}"
-            elif block.type == "thinking":
-                self.output_text += Fore.LIGHTBLACK_EX + block.thinking + f"\n{Fore.RESET}"
+                    if self.usingterminal:
+                        self.output_text += Fore.RESET + block.text + f"\n{Fore.RESET}"
+                    else:
+                        self.output_text += block.text + f"\n"
+            elif block.type == "thinking" and thinking:
+                if self.usingterminal:
+                    self.output_text += Fore.LIGHTBLACK_EX+block.thinking+f"\n{Fore.RESET}"
+                else:
+                    self.output_text += "+"+"-"*18+"+\nThinking:\n"+block.thinking+f"\n+"+"-"*18+"+\n"
         self.messages.append({'role': 'assistant', 'content': self.output_text})
         #print(chat_completion.content) # For debug
         return self.output_text
-    def read_file(self, input):
+    def read_file(self, input, thinking):
         file_path = input['file_path']
         try:
             with open(file_path, 'r', encoding='utf-8') as f:
                 content = f.read()
             self.aout(f"[读取文件 {file_path}]" if self.chinese_mode else f"[Read {file_path}]")
-            self.chat(f'成功读取文件：{file_path}，内容为：{content}', role='system')
+            self.chat(f'成功读取文件：{file_path}，内容为：{content}', role='system', thinking=thinking)
         except FileNotFoundError:
             self.aout(f"[阅读文件失败因为 {file_path} 这个文件不存在]" if self.chinese_mode else f"[Read failed because {file_path} didn't exist]")
-            self.chat(f'文件不存在：{file_path}', role='system')
+            self.chat(f'文件不存在：{file_path}', role='system', thinking=thinking)
         except Exception as e:
             self.aout(f"[阅读文件失败（未知原因）：{file_path}]" if self.chinese_mode else f"[Read failed(Unknown exception) {file_path}]")
-            self.chat(f'读取文件失败：{file_path}，错误信息：{e}', role='system')
-    def edit_file(self, input):
+            self.chat(f'读取文件失败：{file_path}，错误信息：{e}', role='system', thinking=thinking)
+    def edit_file(self, input, thinking):
         file_path = input['file_path']
         content = input['content']
         try:
@@ -134,41 +142,60 @@ class EasyOllamaBeta:
                 with open(file_path, 'w', encoding='utf-8') as f:
                     f.write(content)
                 self.aout(f"[编辑文件 {file_path}]" if self.chinese_mode else f"[Edit {file_path}]")
-                self.chat(f'成功编辑文件：{file_path}', role='system')
+                self.chat(f'成功编辑文件：{file_path}', role='system', thinking=thinking)
             else:
                 with open(file_path, 'w', encoding='utf-8') as f:
                     f.write(content)
                 self.aout(f"[创建文件：{file_path}]" if self.chinese_mode else f"[Created {file_path}]")
-                self.chat(f'成功创建并编辑文件：{file_path}', role='system')
+                self.chat(f'成功创建并编辑文件：{file_path}', role='system', thinking=thinking)
         except Exception as e:
             self.aout(f"[编辑文件失败：{file_path}]" if self.chinese_mode else f"[Edit failed {file_path}]")
-            self.chat(f'编辑文件失败：{file_path}，错误信息：{e}', role='system')
-    def get_time(self, input):
+            self.chat(f'编辑文件失败：{file_path}，错误信息：{e}', role='system', thinking=thinking)
+    def get_time(self, input, thinking):
         if 'format' not in input:
             self.aout("[时间格式错误，未输入时间格式]" if self.chinese_mode else "[Time format error, no format input]")
-            self.chat('时间格式错误，未输入时间格式。', role='system')
+            self.chat('时间格式错误，未输入时间格式。', role='system', thinking=thinking)
         format = input['format']
         try:
             if format == '1':
                 time_str = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                 self.aout(f"[时间获取成功：{time_str}]" if self.chinese_mode else f"[Time get success {time_str}]")
-                self.chat(f'成功获取时间：{time_str}', role='system')
+                self.chat(f'成功获取时间：{time_str}', role='system', thinking=thinking)
             elif format == '2':
                 time_str = datetime.now().strftime('%Y-%m-%d')
                 self.aout(f"[时间获取成功：{time_str}]" if self.chinese_mode else f"[Time get success {time_str}]")
-                self.chat(f'成功获取时间：{time_str}', role='system')
+                self.chat(f'成功获取时间：{time_str}', role='system', thinking=thinking)
             elif format == '3':
                 time_str = datetime.now().strftime('%H:%M:%S')
                 self.aout(f"[时间获取成功：{time_str}]" if self.chinese_mode else f"[Time get success {time_str}]")
-                self.chat(f'成功获取时间：{time_str}', role='system')
+                self.chat(f'成功获取时间：{time_str}', role='system', thinking=thinking)
             else:
                 self.aout(f"[时间格式错误，输入的格式是{format}]" if self.chinese_mode else f"[Time format error, input format is {format}]")
-                self.chat(f'时间格式错误：{format}，请输入1、2、3中的一个。', role='system')
+                self.chat(f'时间格式错误：{format}，请输入1、2、3中的一个。', role='system', thinking=thinking)
         except Exception as e:
             self.aout("[时间获取失败]" if self.chinese_mode else "[Time get failed]")
-            self.chat(f'获取时间失败：{format}，错误信息：{e}', role='system')
+            self.chat(f'获取时间失败：{format}，错误信息：{e}', role='system', thinking=thinking)
     def aout(self, output):
-        self.output_text += Fore.RESET + output + f"\n{Fore.RESET}"
+        if self.usingterminal:
+            self.output_text += Fore.RESET + output + f"\n{Fore.RESET}"
+        else:
+            self.output_text += output + f"\n"
+    def set_api_key(self, api_key):
+        self.api_key = api_key
+        self.reset_cilent()
+    def set_base_url(self, base_url="http://localhost:11434/"):
+        self.base_url = base_url
+        self.reset_cilent()
+    def switch_model(self, model="qwen3.5:0.8b"):
+        self.model = model
+    def reset(self):
+        self.messages = []
+        # 不用重置self.client，因为不影响。
+    def reset_cilent(self):
+        self.client = anthropic.Anthropic(
+            base_url=self.base_url,
+            api_key=self.api_key,  # required but ignored
+        )
 if __name__ == "__main__":
     eob = EasyOllamaBeta(api_key='') # 官网自取API Key。建议使用qwen3.5:0.8b
     while True:
